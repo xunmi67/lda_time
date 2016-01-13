553def6a7a7a25593a2640fe3cdd1c26a5e96f45
toshiba <xunmi67@toshiba.com>
2016-01-13 21:58:49 +0800

    testing...

--- lda/lda.py
+++ lda/lda.py
@@ -22,16 +22,17 @@ class lda():
         X = X if sp.isspmatrix_csr(X) else sp.csr_matrix(X)
         D  = X.shape[0]
         V = X.max()+1
-        z_len = X.sum(1).tonarray()[:,0]
-        z_vectors = [[random.randint(0,self.n_topics-1) for i in range(self.n_topics)] for m in range(D)]
+#        z_len = X.sum(1).getA()[:,0]
+        z_len = [X.indptr[i+1]-X.indptr[i] for i in range(D)]
+        z_vectors = [[random.randint(0,self.n_topics-1) for i in range(z_len[m])] for m in range(D)]
         k_v_mat = sp.csr_matrix(np.zeros((self.n_topics,V),np.int32))
         m_k_mat = sp.csr_matrix(np.zeros((D,self.n_topics),np.int32))
         for d in range(D):
-            indices = X.indices[X.indptr[d],X.indptr[d+1]]
+            d_w = X.data[X.indptr[d]:X.indptr[d+1]]
             z_d = z_vectors[d]
-            assert len(indices) == len(z_d)
-            for i in range(len(indices)):
-                w_i = indices(w_i)
+            assert len(d_w) == len(z_d)
+            for i in range(len(d_w)):
+                w_i = X.data[i]
                 z_i = z_d[i]
                 k_v_mat[z_i][w_i] += 1
                 m_k_mat[d][z_i] += 1
@@ -39,7 +40,7 @@ class lda():
         # gibbs sample,burn in process
         for sample_time in range(self.n_topics*self.MAX_LEN_OF_DOC*20):
             for d in D:
-                w_d = X.indices[X.indptr[d],X.indptr[d+1]]
+                w_d = X.data[X.indptr[d]:X.indptr[d+1]]
                 z_d = z_vectors[d]
                 for i in range(w_d):
                     k_v_mat[z_d[i]][w_d[i]] -= 1
@@ -72,12 +73,13 @@ class lda():
                     for m in range(D)]
         self.phi /= stable_times
         self.theta /= stable_times
+        return self
 
 
-
-
-
-
-
-    def get_parameters(self):
-        pass
+if __name__ == "__main__":
+    x = np.array([ [random.randint(0,499) for i in range(100)] for j in range(1000)])
+    mo = lda(n_topics = 10)
+    mo.fit(x)
+    mo.phi
+    print mo.theta
+    print mo.phi
